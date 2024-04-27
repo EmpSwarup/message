8a2a0f8eb43051c9d7b911f2728c07bfa52a2e19
Swarup Sapkota <empswarupcoc@gmail.com>
2024-04-27 15:21:23 +0545

    changed export name

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -16,7 +16,7 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Loader2 } from "lucide-react"
 
-const page = () => {
+const SignUpPage = () => {
   const [username, setUsername] = useState('')
   const [usernameMessage, setUsernameMessage] = useState('')
   const [isCheckingUsername, setIsCheckingUsername] = useState(false)
@@ -170,4 +170,4 @@ const page = () => {
   )
 }
 
-export default page
+export default SignUpPage
